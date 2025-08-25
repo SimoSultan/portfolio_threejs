fe829b2ed22865c698ffe5ca466cbc327c6382b4
Simon Curran <simosultan2020@gmail.com>
2025-08-26 09:01:49 +1000

    fix: update chatbot index file to resolve test compatibility

--- src/chatbot/index.ts
+++ src/chatbot/index.ts
@@ -1,10 +1,12 @@
 export { Chatbot } from "./chatbot";
 export { ChatUI } from "./chat-ui";
+export { ContextManager } from "./context";
 export { AVAILABLE_MODELS, DEFAULT_MODEL, MODEL_METADATA } from "./models";
 export type { ChatMessage, ModelConfig } from "./chatbot";
+export type { ChatContext, StoredMessage } from "./context";
 export { getOllamaUrl, getOllamaEnvironment, getOllamaConfig } from "./config";
 export type { OllamaConfig } from "./config";
 
 // Database exports
 export { DatabaseManager, StorageManager } from "../database";
-export type { ContextStorage, StoredMessage } from "./context";
+export type { ContextStorage } from "./context";
